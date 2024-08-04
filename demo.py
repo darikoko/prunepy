daf13a6ae8ce72c609a93879e3f3be51cfea1857
Loic <loic@cavinato.fr>
2024-08-04 08:19:33 +0200

    on the good way

--- demo.py
+++ demo.py
@@ -1,19 +1,44 @@
 print("salut")
 
-class Pizza:
-    def __init__(self,name:str, taste:str) -> None:
+
+class Store:
+    def __init__(self, slices: dict[str, object]) -> None:
+        self.slices = slices
+
+    def alert(self, text:str):
+        print(text)
+
+
+class Subject:
+    store: Store | None = None
+    
+    def register_store(self, store:Store):
+        self.store = store
+
+    def notify(self):
+        store.alert("NOTIFICATION")
+
+
+class Pizza(Subject):
+    def __init__(self, name: str, taste: str) -> None:
         self.name = name
         self.taste = taste
 
-class User:
-    def __init__(self, pseudo:str) -> None:
+    def change(self):
+        self.taste = "Salmon"
+        self.notify()
+
+
+class User(Subject):
+    def __init__(self, pseudo: str) -> None:
         self.pseudo = pseudo
 
-class Store:
-    def __init__(self, slices: dict[str, any]) -> None:
-        self.state = state
 
 
-store = Store(
-    {"pizza": {"size": "XL", "taste": "Cheese"}, "user": {"pseudo": "darikol"}}
-)
+
+
+pizza = Pizza("XL", "Peperonni")
+user = User("darikol")
+store = Store({"pizza": pizza, "user": user})
+store.slices["pizza"].change()
+print(store.slices["pizza"].taste)
